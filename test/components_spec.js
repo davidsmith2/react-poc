9a901d7ab348ebf6b4a4c5549c6c5edaeef1b7d0
David Smith <davidsmith2@gmail.com>
2016-10-29 21:57:48 -0400

    Clean up tests

--- test/components_spec.js
+++ test/components_spec.js
@@ -8,156 +8,89 @@ import {mount} from 'enzyme';
 
 import {Spinner, Content, Buttons} from '../src/components';
 
-describe('Spinner', () => {
+describe('Components', () => {
+	const mountOptions = {
+		context: {store: {dispatch() {}}},
+		childContextTypes: {store: React.PropTypes.object}
+	};
 
-	it('is shown when data is loading', () => {
-		const data = {fetching: true};
-		const component = renderIntoDocument(<Spinner data={data} />);
-		const spinner = ReactDOM.findDOMNode(component.refs.spinner);
-		expect(spinner).to.be.ok;
-		expect(spinner.textContent).to.contain('Fetching');
+	describe('Spinner', () => {
+
+		it('is shown when data is loading', () => {
+			const data = {fetching: true};
+			const component = renderIntoDocument(<Spinner data={data} />);
+			const spinner = ReactDOM.findDOMNode(component.refs.spinner);
+			expect(spinner).to.be.ok;
+			expect(spinner.textContent).to.contain('Fetching');
+		});
+
+
+		it('is hidden when data isn\'t loading', () => {
+			const data = {fetching: false};
+			const component = renderIntoDocument(<Spinner data={data} />);
+			const spinner = ReactDOM.findDOMNode(component.refs.spinner);
+			expect(spinner).not.to.be.ok;
+		});
+
 	});
 
+	describe('Content', () => {
 
-	it('is hidden when data isn\'t loading', () => {
-		const data = {fetching: false};
-		const component = renderIntoDocument(<Spinner data={data} />);
-		const spinner = ReactDOM.findDOMNode(component.refs.spinner);
-		expect(spinner).not.to.be.ok;
+		it('should render', () => {
+			const data = {content: {title: 'title', description: 'description'}};
+			const component = mount(<Content data={data} />, mountOptions);
+			expect(component.ref('title').text()).to.equal('title');
+			expect(component.ref('description').text()).to.equal('description');
+		});
+
 	});
 
-});
+	describe('Buttons', () => {
 
-describe('Content', () => {
+		it('should render', () => {
+			const data = {list: {'label': {enabled: false}}};
+			const component = mount(<Buttons data={data} />, mountOptions);
+			expect(component.find('button')).to.have.length(1);
+			expect(component.find('button').at(0).text()).to.equal('label');
+		});
 
-	it('should render', () => {
-		const mountOptions = {
-			context: {
-				store: {
-					dispatch() {}
-				}
-			},
-			childContextTypes: {
-				store: React.PropTypes.object
-			}
-		};
-		const data = {
-			content: {
-				title: 'title',
-				description: 'description'
-			}
-		};
-		const component = mount(<Content data={data} />, mountOptions);
-		expect(component.ref('title').text()).to.equal('title');
-		expect(component.ref('description').text()).to.equal('description');
-	});
+		it('should handle PSAT/NMSQT', () => {
+			const data = {
+				list: {'PSAT/NMSQT': {enabled: false}},
+				assessments: [{assessment: 'PSAT/NMSQT', administered: true}]
+			};
+			const component = mount(<Buttons data={data} />, mountOptions);
+			expect(component.find('button').at(0).prop('disabled')).to.equal(false);
+		});
 
-});
+		it('should handle SAT & SAT Subject', () => {
+			const data = {
+				list: {'SAT & SAT Subject': {enabled: false}},
+				academicYears: [{academicYear: '2018'}]
+			};
+			const component = mount(<Buttons data={data} />, mountOptions);
+			expect(component.find('button').at(0).prop('disabled')).to.equal(false);
+		});
 
-describe('Buttons', () => {
+		it('should handle PSAT/NMSQT', () => {
+			const data = {
+				list: {'PSAT/NMSQT': {enabled: false}},
+				assessments: [{assessment: 'PSAT/NMSQT', administered: true}]
+			};
+			const component = mount(<Buttons data={data} />, mountOptions);
+			expect(component.find('button').at(0).prop('disabled')).to.equal(false);
+		});
 
-	let mountOptions;
+		it('should handle PSAT 8/9', () => {
+			const data = {
+				list: {'PSAT 8/9 - Fall': {enabled: false}},
+				assessments: [{assessment: 'PSAT 8/9', administered: true}],
+				admins: [{admin: 'Fall'}]
+			};
+			const component = mount(<Buttons data={data} />, mountOptions);
+			expect(component.find('button').at(0).prop('disabled')).to.equal(false);
+		});
 
-	beforeEach(() => {
-		mountOptions = {
-			context: {
-				store: {
-					dispatch() {}
-				}
-			},
-			childContextTypes: {
-				store: React.PropTypes.object
-			}
-		};
-	});
-
-	it('should render', () => {
-		const data = {
-			list: {
-				'label': {
-					enabled: false
-				}
-			}
-		};
-		const component = mount(<Buttons data={data} />, mountOptions);
-		expect(component.find('button')).to.have.length(1);
-		expect(component.find('button').at(0).text()).to.equal('label');
-	});
-
-	it('should handle PSAT/NMSQT', () => {
-		const data = {
-			list: {
-				'PSAT/NMSQT': {
-					enabled: false
-				}
-			},
-			assessments: [
-				{
-					assessment: 'PSAT/NMSQT',
-					administered: true
-				}
-			]
-		};
-		const component = mount(<Buttons data={data} />, mountOptions);
-		expect(component.find('button').at(0).prop('disabled')).to.equal(false);
-	});
-
-	it('should handle SAT & SAT Subject', () => {
-		const data = {
-			list: {
-				'SAT & SAT Subject': {
-					enabled: false
-				}
-			},
-			academicYears: [
-				{
-					academicYear: '2018'
-				}
-			]
-		};
-		const component = mount(<Buttons data={data} />, mountOptions);
-		expect(component.find('button').at(0).prop('disabled')).to.equal(false);
-	});
-
-	it('should handle PSAT/NMSQT', () => {
-		const data = {
-			list: {
-				'PSAT/NMSQT': {
-					enabled: false
-				}
-			},
-			assessments: [
-				{
-					assessment: 'PSAT/NMSQT',
-					administered: true
-				}
-			]
-		};
-		const component = mount(<Buttons data={data} />, mountOptions);
-		expect(component.find('button').at(0).prop('disabled')).to.equal(false);
-	});
-
-	it('should handle PSAT 8/9', () => {
-		const data = {
-			list: {
-				'PSAT 8/9 - Fall': {
-					enabled: false
-				}
-			},
-			assessments: [
-				{
-					assessment: 'PSAT 8/9',
-					administered: true
-				}
-			],
-			admins: [
-				{
-					admin: 'Fall'
-				}
-			]
-		};
-		const component = mount(<Buttons data={data} />, mountOptions);
-		expect(component.find('button').at(0).prop('disabled')).to.equal(false);
 	});
 
 });
